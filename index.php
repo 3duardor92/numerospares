89b388d62eda34f934cbbd46b8357817b70f0e7b
Eduardo Rojas <3duardor92@gmail.com>
2022-01-16 11:06:24 -0600

    deje de usar la variabe n

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 $i = 0;     // contardor iniciara desde el numero 0
 $n = 100;   // numero limite del ciclo
-while ($i<=$n){
+while ($i<=100){ //se puede usar sin variable n, que sirva de ejemplo para git
 
 	if(($i%2)== 0){
     	echo $i.", ";
